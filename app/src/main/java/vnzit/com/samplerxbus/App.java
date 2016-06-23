89463ed7104405f39a49dd50365eb2c87cf51ec1
Duong Quang Son <quangson91@gmail.com>
2016-06-23 18:38:51 +0700

    add bus instance to App

--- app/src/main/java/vnzit/com/samplerxbus/App.java
+++ app/src/main/java/vnzit/com/samplerxbus/App.java
@@ -7,13 +7,20 @@ import android.app.Application;
  */
 public class App extends Application {
     private static App instance;
+
+    private RxBus bus;
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+        bus = new RxBus();
     }
 
     public static App get() {
         return instance;
+    }
+
+    public RxBus bus() {
+        return bus;
     }
 }
